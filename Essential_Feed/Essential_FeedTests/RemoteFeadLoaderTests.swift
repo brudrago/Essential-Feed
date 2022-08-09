72dafbbc50dc78243e59a466b7978367905bc8a7
bruna.drago <bruna_drago@hotmail.com>
2022-08-09 08:29:08 -0300

    [Essential]Add RemoteFeedLoader and tests

---
+++ Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
@@ -0,0 +1,53 @@
+import XCTest
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.getFrom(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func getFrom(from url: URL)
+}
+
+class RemoteFeadLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func getFrom(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
