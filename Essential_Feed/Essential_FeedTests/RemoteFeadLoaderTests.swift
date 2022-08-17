016ffd4117716b44ef256098ada7d40c1ac8a953
bruna.drago <bruna_drago@hotmail.com>
2022-08-17 11:48:35 -0300

    [Essential]Combine spy captured params into a single typle (tuple)

--- Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
+++ Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
@@ -35,7 +35,7 @@ class RemoteFeadLoaderTests: XCTestCase {
         var capturedError = [RemoteFeedLoader.Error]()
         
         sut.load { capturedError.append($0) }
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedError, [.connectivity])
         //dessa forma é possível tbm capturar a quantidade de vezes que o erro foi chamado
@@ -50,12 +50,19 @@ class RemoteFeadLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        
+        private var messages = [(url: URL, completion:(Error) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(error)
         }
     }
 }
