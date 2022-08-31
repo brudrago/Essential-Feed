1936e8820a03508ed0e33f48e8c78b9390cc74b5
bruna.drago <bruna_drago@hotmail.com>
2022-08-31 10:30:53 -0300

    [Essential]Add logic to success case and unit test when JSON isempty

--- Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
+++ Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
@@ -58,6 +58,19 @@ class RemoteFeadLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
     }
+    
+    
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyListJSON = Data(bytes: "{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
 
     
     //MARK: - Helpers
