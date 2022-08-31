61caaa8ac6b2dfe8de8c964ff2d893860af79bba
bruna.drago <bruna_drago@hotmail.com>
2022-08-31 12:08:20 -0300

    [Essential] move mapping logic to a new type FeedItemsMapper

--- Essential_Feed/Essential_Feed/FeedFeature/Feed API/RemoteFeedLoader.swift
+++ Essential_Feed/Essential_Feed/FeedFeature/Feed API/RemoteFeedLoader.swift
@@ -32,12 +32,13 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200 ,let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map({ $0.item})))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
-               
+                
             case .failure:
                 completion(.failure(.connectivity))
             }
@@ -45,24 +46,34 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
 
-//representação dos dados da API, pra não quebrar caso o back-end mude algum parametro
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Decodable {
-     let id: UUID
-     let description: String?
-     let location: String?
-     let image: URL
+    //representação dos dados da API, pra não quebrar caso o back-end mude algum parametro
+
+    private struct Item: Decodable {
+         let id: UUID
+         let description: String?
+         let location: String?
+         let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageUrl: image
+            )
+        }
+    }
     
-    var item: FeedItem {
-        return FeedItem(
-            id: id,
-            description: description,
-            location: location,
-            imageUrl: image
-        )
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map{ $0.item }
     }
 }
