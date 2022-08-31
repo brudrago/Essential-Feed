cb930d3d240b19b2ab7a9355b056ee2e8a9d9f04
bruna.drago <bruna_drago@hotmail.com>
2022-08-31 10:38:57 -0300

    [Essential]refactoring test to use expect helper

--- Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
+++ Essential_Feed/Essential_FeedTests/RemoteFeadLoaderTests.swift
@@ -62,14 +62,11 @@ class RemoteFeadLoaderTests: XCTestCase {
     
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
-
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
         
-        let emptyListJSON = Data(bytes: "{\"items\": []}".utf8)
-        client.complete(withStatusCode: 200, data: emptyListJSON)
-        
-        XCTAssertEqual(capturedResults, [.success([])])
+         expect(sut, toCompleteWith: .success([]), when: {
+            let emptyListJSON = Data(_ : "{\"items\": []}".utf8)
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        })
     }
 
     
