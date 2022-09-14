82144116691e365755360531d3dadb45cdb1b006
bruna.drago <bruna_drago@hotmail.com>
2022-09-14 09:36:23 -0300

    [Essential] Move mapper logic to feedItemsMapper

--- Essential_Feed/Essential_Feed/FeedFeature/Feed API/FeedItemsMapper.swift
+++ Essential_Feed/Essential_Feed/FeedFeature/Feed API/FeedItemsMapper.swift
@@ -3,16 +3,20 @@ import Foundation
 internal final class FeedItemsMapper { //internal pra nao ser acessivel de outros modulos
     private struct Root: Decodable {
         let items: [Item]
+        
+        var feed: [FeedItem] {
+            return items.map{ $0.item }
+        }
     }
-
+    
     //representação dos dados da API, pra não quebrar caso o back-end mude algum parametro
     //decouple
-
+    
     private struct Item: Decodable {
-         let id: UUID
-         let description: String?
-         let location: String?
-         let image: URL
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
         
         var item: FeedItem {
             return FeedItem(
@@ -26,10 +30,11 @@ internal final class FeedItemsMapper { //internal pra nao ser acessivel de outro
     
     private static var OK_200StatusCode: Int { return 200 }
     
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200StatusCode else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        return try JSONDecoder().decode(Root.self, from: data).items.map{ $0.item }
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200StatusCode,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+                  return .failure(.invalidData)
+              }
+        return(.success(root.feed))
     }
 }
