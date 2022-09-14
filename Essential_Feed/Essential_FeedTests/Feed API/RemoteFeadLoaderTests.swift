5c80ae9bf7cb4d2a5a71fa9d1dded57283fe7ad7
bruna.drago <bruna_drago@hotmail.com>
2022-09-14 10:40:43 -0300

    [ESSENTIAL]Create helper method failure to simplify tests

--- Essential_Feed/Essential_FeedTests/Feed API/RemoteFeadLoaderTests.swift
+++ Essential_Feed/Essential_FeedTests/Feed API/RemoteFeadLoaderTests.swift
@@ -32,7 +32,7 @@ class RemoteFeadLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteWith: failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -43,7 +43,7 @@ class RemoteFeadLoaderTests: XCTestCase {
         let wrongStatusCode = [199, 201, 300, 400, 500]
         
         wrongStatusCode.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             }
@@ -54,7 +54,7 @@ class RemoteFeadLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
        
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWith: failure(.invalidData)) {
             let invalidJSON = Data(_ :"invalidJSON".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
@@ -118,6 +118,10 @@ class RemoteFeadLoaderTests: XCTestCase {
         trackForMemoryLeaks(sut)
         trackForMemoryLeaks(client)
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
