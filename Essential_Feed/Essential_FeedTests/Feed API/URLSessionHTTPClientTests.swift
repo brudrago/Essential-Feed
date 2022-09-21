3426ac14a29665371d31106505258c676f1c412d
Bruna Drago <bruna.drago@ifood.com.br>
2022-09-21 10:22:08 -0300

    [ESSENTIAL]Create URLSession data task with URL on URLSessionHTTPClient.get(from: URL)

---
+++ Essential_Feed/Essential_FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+
+final class URLSessionHTTPClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) {_, _, _ in}
+    }
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLS, [url])
+    }
+    
+    //MARK: - helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLS = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLS.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+
+}
+
+
