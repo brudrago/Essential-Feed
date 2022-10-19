6e420ade080b6dc61bf8285da1602e5a79373ef5
Bruna Drago <bruna.drago@ifood.com.br>
2022-10-19 10:30:48 -0300

    [Essential]Move the creation of SUT to a factory method

--- Essential_Feed/Essential_FeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ Essential_Feed/Essential_FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -44,7 +44,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in}
+        makeSUT().get(from: url) { _ in}
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -54,11 +54,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
@@ -71,7 +69,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    //MARK: - helpers
+    //MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
