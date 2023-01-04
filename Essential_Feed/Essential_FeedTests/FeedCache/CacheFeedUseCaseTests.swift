e2aff1119d43de2d1e6c14992b9e18618417f794
Bruna Drago <bruna.drago@ifood.com.br>
2023-01-04 10:31:14 -0300

    [Essential]Move LocalFeedLoader and FeedStore protocol to its own production file

--- Essential_Feed/Essential_FeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -1,43 +1,6 @@
 import Essential_Feed
 import XCTest
 
-class LocalFeedLoader {
-    private let store: FeedStore
-    private let currentDate: () -> Date
-    
-    init(store: FeedStore, currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    
-    func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [weak self] error in
-            guard let self = self else { return }
-            
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(items, completion: completion)
-            }
-        }
-    }
-    
-    private func cache(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.insert(items, timestamp: currentDate()) { [weak self] error in
-            guard self != nil else { return }
-            completion(error)
-        }
-    }
-}
-
-protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsetionCompletion = (Error?) -> Void
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsetionCompletion)
-}
-
 final class CacheFeedUseCaseTests: XCTestCase {
 
     func test_doesNotMessageStoreUponCreation() {
