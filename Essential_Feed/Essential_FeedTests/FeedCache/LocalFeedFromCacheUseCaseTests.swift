ef0802ce13c81d6a5251aa17648229121a66f95f
Bruna Drago <bruna.drago@ifood.com.br>
2023-01-25 19:33:57 -0300

    [Essential]Create LocalFeedFromCache tests file + LocalFeedLoader does not message store upon creation(before loading the cache store)
    wq

--- Essential_Feed/Essential_FeedTests/FeedCache/LocalFeedFromCacheUseCaseTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/LocalFeedFromCacheUseCaseTests.swift
@@ -1,35 +1,21 @@
-//
-//  LocalFeedFromCacheUseCaseTests.swift
-//  Essential_FeedTests
-//
-//  Created by bruna.drago on 25/01/23.
-//
-
+import Essential_Feed
 import XCTest
 
 final class LocalFeedFromCacheUseCaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    //MARK: Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
