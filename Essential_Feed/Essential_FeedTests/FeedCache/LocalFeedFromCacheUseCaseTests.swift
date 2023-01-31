475abe5b3bc10009a5b49aa6a92b58b1da8d4cd9
Bruna Drago <bruna.drago@ifood.com.br>
2023-01-31 09:36:38 -0300

    [Essential]Load command delivers cached images on less then 7 days old cache

--- Essential_Feed/Essential_FeedTests/FeedCache/LocalFeedFromCacheUseCaseTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/LocalFeedFromCacheUseCaseTests.swift
@@ -33,6 +33,16 @@ final class LocalFeedFromCacheUseCaseTests: XCTestCase {
         }
     }
     
+    func test_load_deliversCacheImagesOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT (currentDate: { fixedCurrentDate })
+        expect(sut, toCompleteWith: .success(feed.models)) {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        }
+    }
+    
     //MARK: Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -65,5 +75,29 @@ final class LocalFeedFromCacheUseCaseTests: XCTestCase {
     
     private func anyNSError() -> NSError {
         return NSError(domain: "any error", code: 0)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+    private func uniqueImage() -> FeedImage {
+        FeedImage(id: UUID(), description: "description", location: "location", url: anyURL())
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueImage(), uniqueImage()]
+        let localItems = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
+        return (models, localItems)
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
