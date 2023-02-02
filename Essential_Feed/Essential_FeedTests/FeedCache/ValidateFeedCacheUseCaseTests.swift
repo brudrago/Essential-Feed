6b1860f57c23f37808bc27c1d31f3c20dad1ce07
Bruna Drago <bruna.drago@ifood.com.br>
2023-02-02 08:57:31 -0300

    [Essential]Create ValidateFeedcacheUseCase file to separate the query from its side effects

---
+++ Essential_Feed/Essential_FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+//Esse use case vai ser criado pra separar a lógica de validar/deletar da lógica de Load Cache
+//Principio de Separating Queries & Side Effetcs
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+
+}
