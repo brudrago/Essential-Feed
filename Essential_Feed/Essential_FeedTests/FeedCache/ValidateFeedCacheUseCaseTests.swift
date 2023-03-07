083a422c69859575c2c71bad79f2821f7063ad5d
Bruna Drago <bruna.drago@ifood.com.br>
2023-03-07 08:17:47 -0300

    [ESSENTIAL]Create ValidateFeedCacheUseCase

--- Essential_Feed/Essential_FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -1,9 +1,23 @@
+import Essential_Feed
 import XCTest
 
 //Esse use case vai ser criado pra separar a lógica de validar/deletar da lógica de Load Cache
 //Principio de Separating Queries & Side Effetcs
 
 final class ValidateFeedCacheUseCaseTests: XCTestCase {
-
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
 
 }
