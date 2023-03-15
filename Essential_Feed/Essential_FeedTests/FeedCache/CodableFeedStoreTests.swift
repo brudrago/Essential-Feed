5bf9a7d16ee4c095af2015cd228871f25276ba1a
Bruna Drago <bruna.drago@ifood.com.br>
2023-03-15 11:18:26 -0300

    [ESSENTIAL]Create CodableFeedStore tests + retrieve from empty cache delivers result

---
+++ Essential_Feed/Essential_FeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -0,0 +1,30 @@
+import Essential_Feed
+import XCTest
+
+final class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+final class CodableFeedStoreTests: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
