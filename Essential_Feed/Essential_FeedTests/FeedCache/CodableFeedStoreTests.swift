6ae1cc2dea4ec204ca619b6297d7ede4c1bf7fb9
Bruna Drago <bruna.drago@ifood.com.br>
2023-03-22 09:04:05 -0300

    [ESSENTIAL]Add helper methods to provide documentation, context and clarify test setUP and tearDown intent regarg side effects

--- Essential_Feed/Essential_FeedTests/FeedCache/CodableFeedStoreTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -5,12 +5,12 @@ final class CodableFeedStoreTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -90,5 +90,17 @@ extension CodableFeedStoreTests {
     
     func testSpecificStoreURL() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
