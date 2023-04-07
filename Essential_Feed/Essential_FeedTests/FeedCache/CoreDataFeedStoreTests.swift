b3cc6f7c791172a7481076a050ff0e2282880c48
Bruna Drago <bruna.drago@ifood.com.br>
2023-04-07 17:59:55 -0300

    [ESSENTIAL]Proved that CoreDataFeedStore side-effects run serially

--- Essential_Feed/Essential_FeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -61,7 +61,9 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
         
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // - MARK: Helpers
