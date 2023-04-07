f841d375fa6afc914edd7ac249756265525e711e
Bruna Drago <bruna.drago@ifood.com.br>
2023-04-07 17:55:00 -0300

    [ESSENTIAL] CoreDataFeedStore.deleteCachedFeed() + tests

--- Essential_Feed/Essential_FeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ Essential_Feed/Essential_FeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -41,16 +41,17 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        
-        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
