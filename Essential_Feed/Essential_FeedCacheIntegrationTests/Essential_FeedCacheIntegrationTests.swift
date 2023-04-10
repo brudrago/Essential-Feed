962f316ad4f4a0e74d80d344495b4d5cf34c2926
Bruna Drago <bruna.drago@ifood.com.br>
2023-04-10 19:48:21 -0300

    [ESSENTIAL] Add setup and teardow to clean states between every tests + test local feed loader delivers item saved

--- Essential_Feed/Essential_FeedCacheIntegrationTests/Essential_FeedCacheIntegrationTests.swift
+++ Essential_Feed/Essential_FeedCacheIntegrationTests/Essential_FeedCacheIntegrationTests.swift
@@ -4,6 +4,13 @@ import XCTest
 //problema de não identificar modulo parou quando mexi no Build Settings, supported plataforms e coloquei iOS
 
 final class Essential_FeedCacheIntegrationTests: XCTestCase {
+    override func setUp() {
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        undoStoreSideEffects()
+    }
 
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
@@ -21,6 +28,34 @@ final class Essential_FeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
+
     
     //-MARK: - Helpers
     
@@ -40,6 +75,19 @@ final class Essential_FeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
 }
