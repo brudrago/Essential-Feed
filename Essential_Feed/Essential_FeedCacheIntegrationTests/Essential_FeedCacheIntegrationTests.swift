08057407ac9866f4596c13cc4dbe4acc15f61674
Bruna Drago <bruna.drago@ifood.com.br>
2023-04-10 19:24:09 -0300

    [ESSENTIAL] Add EssentialFeedCacheIntegrationTests target to separate the potentially slower cache integration tests from the fast unit/isolated tests

--- Essential_Feed/Essential_FeedCacheIntegrationTests/Essential_FeedCacheIntegrationTests.swift
+++ Essential_Feed/Essential_FeedCacheIntegrationTests/Essential_FeedCacheIntegrationTests.swift
@@ -1,7 +1,12 @@
+import Essential_Feed
 import XCTest
+
+//problema de não identificar modulo parou quando mexi no Build Settings, supported plataforms e coloquei iOS
 
 final class Essential_FeedCacheIntegrationTests: XCTestCase {
 
-
+    func test() {
+        //XCTFail("should fail")
+    }
 
 }
