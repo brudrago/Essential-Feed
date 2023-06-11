7c3617ddc6501e8e577861da7eedb67f6dde71a8
Bruna Drago <bruna.drago@ifood.com.br>
2023-06-11 16:14:04 -0300

    [Essential]Replace LoaderSpy reference on production code by the production FeedLoader protocol

--- Essential_Feed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ Essential_Feed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,16 +1,17 @@
 import XCTest
 import UIKit
+import Essential_Feed
 
 final class FeedViewController: UIViewController {
-    private var loader: FeedViewControllerTests.LoaderSpy?
+    private var loader: FeedLoader?
     
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+    convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
     
     override func viewDidLoad() {
-        loader?.load()
+        loader?.load { _ in }
     }
 }
 
@@ -34,10 +35,10 @@ final class FeedViewControllerTests: XCTestCase {
     
     //MARK: - Helpers
     
-    class LoaderSpy {
+    class LoaderSpy: FeedLoader {
         private (set) var loadCallCount: Int = 0
         
-        func load() {
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
         }
     }
