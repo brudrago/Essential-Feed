2fdc2a87ae21772f5fc8c35d8d5afe946a40e58c
Bruna Drago <bruna.drago@ifood.com.br>
2023-06-21 11:43:05 -0300

    [Essential]Load when image view is visible (on screen)

--- Essential_Feed/EssentialFeediOS/FeedViewController.swift
+++ Essential_Feed/EssentialFeediOS/FeedViewController.swift
@@ -1,13 +1,22 @@
 import UIKit
 import Essential_Feed
 
+//Adding FeedImageDataLoader protocol abstraction, we decouple the ViewController from concrete implementations like URlSession
+//ViewController doesn`t care where the image data comes from (e.g, cache or network), this way we are free to change the implementation or add more funcionalities on demand without having to modify the controller (open/close principle)
+
+public protocol FeedImageDataLoader {
+    func loadImageData(from url: URL)
+}
+
 public final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
+    private var feedLoader: FeedLoader?
+    private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
     
-   public  convenience init(loader: FeedLoader) {
+   public  convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader?) {
         self.init()
-        self.loader = loader
+        self.feedLoader = feedLoader
+       self.imageLoader = imageLoader
     }
     
     public override func viewDidLoad() {
@@ -18,7 +27,7 @@ public final class FeedViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] result in
+        feedLoader?.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.tableModel = feed
                 self?.tableView.reloadData()
@@ -37,6 +46,7 @@ public final class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil)
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
+        imageLoader?.loadImageData(from: cellModel.url)
         return cell
     }
 }
