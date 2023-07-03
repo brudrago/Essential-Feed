8d8b39909644c74c881a5c4d390f3a2fc6501fa4
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-03 13:49:12 -0300

    [Essential]Comment test failed CI

--- Essential_Feed/EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
+++ Essential_Feed/EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -6,7 +6,6 @@ public final class FeedImageCell: UITableViewCell {
     public let descriptionLabel = UILabel()
     public let feedImageContainer = UIView()
     public let feedImageView = UIImageView()
-    //public let feedImageRetryButton = UIButton()
     
     private(set) public lazy var feedImageRetryButton: UIButton = {
         let button = UIButton()
