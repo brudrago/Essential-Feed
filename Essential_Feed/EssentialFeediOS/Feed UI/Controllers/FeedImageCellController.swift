ea0f284ef964af31d34b6a37ab0e69935591aa17
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-03 11:26:21 -0300

    [Essential]Extract FeedImageCell creation/configuration and image loading logic with the new FeedImageCellController

---
+++ Essential_Feed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,49 @@
+import UIKit
+import Essential_Feed
+
+final class FeedImageCellController {
+    private var task: FeedImageDataLoaderTask?
+    private var imageLoader: FeedImageDataLoader
+    private var model: FeedImage
+    
+    //instead to use method injection, we use init injection because values this class dont need to manage more than 1 model or loader, this is decided on creation
+   init(imageLoader: FeedImageDataLoader, model: FeedImage) {
+        self.imageLoader = imageLoader
+        self.model = model
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+        
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+            
+            self.task = self.imageLoader.loadImageData(from: self.model.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+        
+        cell.onRetry = loadImage
+        loadImage()
+        
+        return cell
+    }
+    
+    func preLoad() {
+        task = imageLoader.loadImageData(from: model.url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
