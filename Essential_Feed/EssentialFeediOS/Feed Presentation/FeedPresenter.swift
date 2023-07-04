b4dc4b11000bdc48540b43d8037649f06f7e2c21
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-04 10:57:30 -0300

    [Essential]Move memory management to Composer layer, FeedPresenter should not have to know or handle their dependencies lifetime

--- Essential_Feed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ Essential_Feed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -4,7 +4,7 @@ protocol FeedView {
     func display(feed: [FeedImage])
 }
 
-protocol FeedLoadingView: class {
+protocol FeedLoadingView {
     func display(isLoading: Bool)
 }
 
@@ -20,7 +20,7 @@ final class FeedPresenter {
     }
     
     var feedView: FeedView?
-    weak var loadingView: FeedLoadingView?
+    var loadingView: FeedLoadingView?
    
     
     func loadFeed() {
--- Essential_Feed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ Essential_Feed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -8,11 +8,30 @@ public final class FeedUIComposer {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(presenter: feedPresenter)
         let feedController = FeedViewController(refreshController: refreshController)
-        feedPresenter.loadingView = refreshController
+        feedPresenter.loadingView = WeakRefVirtualProxy(object: refreshController)
         feedPresenter.feedView = FeedViewAdapter(controller: feedController, loader: imageLoader)
         return feedController
     }
 }
+
+//In this part we bring memory management to the Compose layer, instead of let on Presenter
+//WeakRefVirtualProxy will hold an weak reference of the object instance and pass the message forward
+//when we set loading view, we weakfy with VirtualProxy
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
+
 
 //[FeedImage] -> Adapt -> [FeedImageCellController]
 
