3fe32fd21a87eaf27edef251ec44e47aee4dea0d
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-04 18:07:05 -0300

    [Essential]Delete unncessary comment and set VC title

--- Essential_Feed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ Essential_Feed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -15,6 +15,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public override func viewDidLoad() {
+        title = "My Feed"
         refresh()
     }
     
