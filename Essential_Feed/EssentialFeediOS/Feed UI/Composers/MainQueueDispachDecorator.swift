24154d29aaca43bc698cffdc0f5a57b105e63700
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-05 10:06:07 -0300

    [Essential]Change decorator to be generic

--- Essential_Feed/EssentialFeediOS/Feed UI/Composers/MainQueueDispachDecorator.swift
+++ Essential_Feed/EssentialFeediOS/Feed UI/Composers/MainQueueDispachDecorator.swift
@@ -1,24 +1,49 @@
 import Essential_Feed
 import UIKit
 
-//Adding behavior without change code, adding behavior to an instance keeping same interface
-final class MainQueueDispachDecorator: FeedLoader {
+//Generic Decorator
+final class MainQueueDispachDecorator<T> {
 
-    private let decoratee: FeedLoader
+    private let decoratee: T
     
-    init(decoratee: FeedLoader) {
+    init(decoratee: T) {
         self.decoratee = decoratee
     }
     
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+}
+
+extension MainQueueDispachDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
 }
+
+//Adding behavior without change code, adding behavior to an instance keeping same interface
+//final class MainQueueDispachDecorator: FeedLoader {
+//
+//    private let decoratee: FeedLoader
+//
+//    init(decoratee: FeedLoader) {
+//        self.decoratee = decoratee
+//    }
+//
+//    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+//        decoratee.load { result in
+//            if Thread.isMainThread {
+//                completion(result)
+//            } else {
+//                DispatchQueue.main.async {
+//                    completion(result)
+//                }
+//            }
+//        }
+//    }
+//}
